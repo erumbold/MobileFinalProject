983ca538cc375d9ffe667a8f177d929adcac2b3b
Andrew Fabbro <andrewmfabbro@gmail.com>
2016-12-06 20:36:13 -0500

    Worked on Shared Preferences for ProfileActivity.java

--- app/src/main/java/com/rumboldfabbro/mobilefinalproject/ProfileActivity.java
+++ app/src/main/java/com/rumboldfabbro/mobilefinalproject/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.rumboldfabbro.mobilefinalproject;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -19,6 +20,7 @@ public class ProfileActivity extends Activity {
     private EditText name, school;
     private Spinner major;
     private Button saved_list;
+    private SharedPreferences.Editor myEd;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,13 +31,18 @@ public class ProfileActivity extends Activity {
         school = (EditText) findViewById(R.id.profile_school);
         major = (Spinner) findViewById(R.id.profile_major);
         saved_list = (Button) findViewById(R.id.profile_savedlist);
+        final SharedPreferences userPref = getApplicationContext().getSharedPreferences("MyPreferences", MODE_PRIVATE);
+
 
         loadSpinnerData();
+
+        myEd = userPref.edit();
 
         name.setOnFocusChangeListener(new View.OnFocusChangeListener(){
             @Override
             public void onFocusChange(View v, boolean hasFocus){
                 //TODO change user's name in saved preferences
+                myEd.putString("name", name.getText().toString());
             }
         });
 
@@ -43,16 +50,23 @@ public class ProfileActivity extends Activity {
             @Override
             public void onFocusChange(View v, boolean hasFocus){
                 //TODO change user's high school in saved preferences
+                myEd.putString("school", school.getText().toString());
             }
         });
 
         saved_list.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+
                 //TODO activate AboutActivity
+                //save list to preferences
+                myEd.commit();
+
             }
         });
     }
+
+
 
     public void loadSpinnerData() {
         Database db = new Database(getApplicationContext());
@@ -66,6 +80,10 @@ public class ProfileActivity extends Activity {
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
         String major = parent.getItemAtPosition(position).toString();
         //TODO change user's intended major in saved preferences
+
+        //what do we use to get a string from the spinner?
+        myEd.putString("major", major);
+
     }
 
 }
